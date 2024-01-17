fe430e579551cf8dfacabbeacff5f11c7736ad35
Bula0023 <146153839+Bula0023@users.noreply.github.com>
2024-01-16 22:50:35 -0600

    Create fib.ml

---
+++ fib.ml
@@ -0,0 +1,10 @@
+(* Author: Eric Van Wyk
+    Modified by: Horoom *)
+
+(* A function computing the Fibonacci sequence: 1, 1, 2, 3, 5, 8, ... *)
+
+(* There is a bug in teh following program. Caan you fix it? *)
+
+
+let rec fib x =
+    if x < 3 then 1 else fib (n-1) + fib (n-1)
